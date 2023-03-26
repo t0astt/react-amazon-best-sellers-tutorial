e4188621f56f1fdce3657157d9e79133a836699d
t0astt <t0astt93@gmail.com>
2023-03-26 16:19:41 -0500

    Remove span

--- src/Book.js
+++ src/Book.js
@@ -1,14 +1,13 @@
 import React from "react";
 
 const Book = (props) => {
-    const {author, title, img, number} = props;  // destructuring props into vars.
+    const {author, title, img} = props;  // destructuring props into vars.
 
     return (
         <div className="book">
             <img src={img} alt={title}/>
             <h2>{title}</h2>
             <h4>{author}</h4>
-            <span className="number">{`# ${number + 1}`}</span>
         </div>
     )
 
